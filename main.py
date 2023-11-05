fb1a19606046d168eadefc2fdd2f7fa8fdf8c3bf
Avery Wood <38259608+AveryWood@users.noreply.github.com>
2023-11-04 20:40:11 -0700

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ def find_match(arr,choose_k):
         sample_image, label = mnist_train[i]
         sample_image_arr = np.array(sample_image, dtype=np.int32)
         distance = np.sqrt(sum((arr - sample_image_arr)**2))
-        distances[i] = mnist_train[i][1], distance
-        actual_labels[i] = label
+        distances[i] = distance
+        
         
     sorted_distances = np.argsort(distances)
     k_elements = sorted_distances[:choose_k]
@@ -26,7 +26,12 @@ def find_match(arr,choose_k):
 
     return torch.mode(k_elements).values.item(0)
 
-for i in range(1000):
-    find_match(np.array(mnist_test[1]), 5)
+num_trials = 500
+actual_labels   = np.empty(500, dtype=object)
+predicted_labels = np.empty(500, dtype=object)
 
-print("Accuracy: " + sum(actual_label == predicted_labels) / 1000 * 100)
+for i in range(num_trials):
+    predicted_labels[i] = find_match(np.array(mnist_test[i]), 5)
+    actual_label[i] = mnist_test[i][1]
+
+print("Accuracy: " + sum(actual_label == predicted_labels) / 1000 * 100 + "%")
