7aa75d0c600527c1d9673c3142e0cdce3a98b6bc
Avery Wood <38259608+AveryWood@users.noreply.github.com>
2024-11-11 20:25:24 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
 import random
-import cv2
 import numpy as np
 import torchvision
 import torch
-import PIL
 
+# Download MNIST data
 mnist_train = torchvision.datasets.MNIST(root='./data', train=True, download=True)
 mnist_test = torchvision.datasets.MNIST(root='./data', train=False, download=True)
 
-
 def find_match(arr, choose_k):
-    distances = np.full(len(mnist_train), np.inf) 
+    distances = np.full(len(mnist_train), np.inf)
     random_indices = random.sample(range(len(mnist_train)), 1000)
     for i in random_indices:
         sample_image, label = mnist_train[i]
@@ -22,17 +20,21 @@ def find_match(arr, choose_k):
     k_indices = sorted_distances[:choose_k]
     predicted_labels = [mnist_train[i][1] for i in k_indices]
 
-    return np.bincount(k_indices).argmax()
+    return np.bincount(predicted_labels).argmax()
 
-num_trials = 500
-actual_labels = np.empty(1000, dtype=object)
-predicted_labels = np.empty(1000, dtype=object)
+def main():
+    num_trials = 500
+    actual_labels = np.empty(1000, dtype=object)
+    predicted_labels = np.empty(1000, dtype=object)
 
-for i in range(num_trials):
-    input_image, true_label = mnist_test[i]
-    predicted_label = find_match(np.array(input_image), 5)
-    actual_labels[i] = true_label
-    predicted_labels[i] = predicted_label
+    for i in range(num_trials):
+        input_image, true_label = mnist_test[i]
+        predicted_label = find_match(np.array(input_image), 5)
+        actual_labels[i] = true_label
+        predicted_labels[i] = predicted_label
 
-accuracy = (sum(actual_labels[i] == predicted_labels[i] for i in range(500)) / 500) * 100
-print("Accuracy: {:.2f}%".format(accuracy))
+    accuracy = (sum(actual_labels[i] == predicted_labels[i] for i in range(num_trials)) / num_trials) * 100
+    print("Accuracy: {:.2f}%".format(accuracy))
+
+if __name__ == "__main__":
+    main()
